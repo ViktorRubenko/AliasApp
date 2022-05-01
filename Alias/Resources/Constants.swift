fa6579b962da51518f3f5e1b2dacec7654eceb01
Victor Rubenko <vrubenko@gmail.com>
2022-05-02 01:42:24 +0300

    Add Colors to Assets

--- Alias/Resources/Constants.swift
+++ Alias/Resources/Constants.swift
@@ -10,12 +10,12 @@ import UIKit
 
 enum Constants {
     enum Colors {
-        static let mainBackgroundColor = UIColor(red: 213/255, green: 218/255, blue: 224/255, alpha: 1)
-        static let secondaryBackgrounColor = UIColor(red: 74/255, green: 110/255, blue: 136/255, alpha: 1)
-        static let navBarColor = secondaryBackgrounColor
-        static let textColor = UIColor(red: 1/255, green: 12/255, blue: 30/255, alpha: 1)
-        static let secondaryTextColor = mainBackgroundColor
-        static let bottomButtonColor = UIColor(red: 62/255, green: 156/255, blue: 190/255, alpha: 1)
-        static let bottomBackgroundColor = UIColor(red: 136/255, green: 162/255, blue: 187/255, alpha: 1)
+        static let mainBackgroundColor = UIColor(named: "mainBackgroundColor")
+        static let secondaryBackgrounColor = UIColor(named: "secondaryBackgrounColor")
+        static let navBarColor = UIColor(named: "navBarColor")
+        static let textColor = UIColor(named: "textColor")
+        static let secondaryTextColor = UIColor(named: "secondaryTextColor")
+        static let bottomButtonColor = UIColor(named: "bottomButtonColor")
+        static let bottomBackgroundColor = UIColor(named: "bottomBackgroundColor")
     }
 }
