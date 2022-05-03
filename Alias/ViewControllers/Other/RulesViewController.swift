9276616511650905b5782425903a1d8cf944da21
daniilSimakhin <danil.110@yandex.ru>
2022-05-03 23:43:19 +0700

    Create rules view

--- Alias/ViewControllers/Other/RulesViewController.swift
+++ Alias/ViewControllers/Other/RulesViewController.swift
@@ -7,23 +7,69 @@
 
 import UIKit
 
-class RulesViewController: UIViewController {
-
+class RulesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let identifire = "MyCell"
+    let rules = [
+        Rule(text: "Задача каждого игрока - объяснить как можно больше слов товарищам по команде за ограниченное время."),
+        Rule(text: "Во время объяснения нельзя использовать однокоренные слова, озвучивать перевод иностранных языков"),
+        Rule(text: "Отгаданное слово приносит команде одно очко, а за пропущенное слово команда штрафуется (в зависимости от настроек)."),
+        Rule(text: "Победителем становится команда, у которой количество очков достигло заранее установленного значения."),
+    ]
+    
+    lazy var headerView: UIImageView = {
+        let view = UIImageView()
+        view.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        view.center.x = tableView.center.x
+        view.image = UIImage(named: "logo")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        createTable()
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:  10),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+        
+        ])
+        title = "Правила"
+        view.backgroundColor = Constants.Colors.mainBackgroundColor
+        tableView.tableHeaderView = headerView
     }
-    */
+    
+    func createTable() {
+        self.tableView = UITableView(frame: view.bounds, style: .plain)
+        self.tableView.register(RuleTableViewCell.self, forCellReuseIdentifier: RuleTableViewCell.identifier)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.sectionHeaderHeight = 140
+        tableView.allowsSelection = false
+    }
+    //MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rules.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RuleTableViewCell.identifier, for: indexPath) as! RuleTableViewCell
+        cell.labelRule.text = rules[indexPath.row].text
+        cell.imageRule.image = UIImage(systemName: "i.circle")
+        cell.backgroundColor = .clear
+        return cell
+    }
 }
