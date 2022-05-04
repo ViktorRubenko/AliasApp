3f87d5377b85eaa6aea1c28133149863848dd5f7
daniilSimakhin <danil.110@yandex.ru>
2022-05-04 14:20:00 +0700

    Create bottomButton in gameSettingsViewController

--- Alias/ViewControllers/Other/GameSettingsViewController.swift
+++ Alias/ViewControllers/Other/GameSettingsViewController.swift
@@ -5,17 +5,58 @@
 //  Created by Victor Rubenko on 01.05.2022.
 //
 
+import SwiftUI
 import UIKit
 
 class GameSettingsViewController: UIViewController {
 
+    private var gameService: GameServiceProtocol!
+    private var componentsFactory: ComponentsBaseFactory!
+    
+    private lazy var bottomButton: UIButton = {
+        let button = componentsFactory.bottomButton()
+        button.setTitle("Далее", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        //button.addTarget(self, action: #selector(didTapBottomButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private let bottomButtonContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.Colors.bottomButtonColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(bottomButtonContainer)
+        bottomButtonContainer.addSubview(bottomButton)
 
+        NSLayoutConstraint.activate([
+            bottomButtonContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomButtonContainer.leadingAnchor.constraint(equalTo: bottomButton.leadingAnchor),
+            bottomButtonContainer.trailingAnchor.constraint(equalTo: bottomButton.trailingAnchor),
+            bottomButtonContainer.topAnchor.constraint(equalTo: bottomButton.topAnchor),
+            
+            bottomButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomButton.heightAnchor.constraint(equalToConstant: Constants.Sizes.BottomButton.height),
+        ])
         // Do any additional setup after loading the view.
     }
     
-
+    init(gameService: GameServiceProtocol, componentsFactory: ComponentsBaseFactory) {
+        self.componentsFactory = componentsFactory
+        self.gameService = gameService
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     /*
     // MARK: - Navigation
 
@@ -26,4 +67,22 @@ class GameSettingsViewController: UIViewController {
     }
     */
 
+    struct SwiftUIController: UIViewControllerRepresentable {
+        typealias UIViewControllerType = GameSettingsViewController
+        
+        func makeUIViewController(context: Context) -> UIViewControllerType {
+            let viewController = UIViewControllerType(gameService: GameServiceForTest.shared, componentsFactory: ComponentFactory())
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+    
+    struct SwiftUIController_Previews: PreviewProvider {
+        static var previews: some View {
+            SwiftUIController().edgesIgnoringSafeArea(.all)
+        }
+    }
 }
